78324593cb92be9fb1161ab7ce6d8bbd03b88c3d
Bas <bas.vanseeters@hotmail.com>
2020-11-03 20:37:11 +0100

    Initial commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float horSpeed;
+    public float vertSpeed;
+    public float jumpPower;
+    public Rigidbody rigidBody;
+    public GameObject playerObject;
+
+    public float width = Screen.width;
+    public bool isGrounded;
+
+    public Animator anim;
+    public GameObject playerArm;
+    public GameObject heldItem;
+    public GameObject weapon;
+    public int tempDamage; // vervang deze later met fatsoenlijke OOP weapons en plant damage in weapon zelf!
+    public bool isAttacking;
+
+    public bool canInteract;
+    IShowText iText;
+
+
+
+    bool canSlice;
+
+    Vector3 playerScreenSpace;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = playerArm.GetComponent<Animator>();
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+        playerScreenSpace = Camera.main.WorldToScreenPoint(playerObject.transform.position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertTrans = Input.GetAxis("Vertical") * vertSpeed;
+        float horTrans = Input.GetAxis("Horizontal") * horSpeed;
+
+        vertTrans *= Time.deltaTime;
+        horTrans *= Time.deltaTime;
+
+        transform.Translate(horTrans, 0, vertTrans);
+
+
+        float DisstanceToTheGround = GetComponent<Collider>().bounds.extents.y;
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, DisstanceToTheGround + 0.1f);
+
+
+        if (Input.GetKeyDown("space") && isGrounded)
+        {
+            //rigidBody.AddForce(new Vector3(0, jumpPower, 0));
+            rigidBody.velocity = rigidBody.velocity + new Vector3(0, jumpPower, 0);
+        }
+
+        if(Input.GetMouseButtonDown(0) && !isAttacking)
+        {
+            anim.SetTrigger("Attack");
+            StartCoroutine(attackBool());
+        }
+
+        if (Input.mousePosition.x < playerScreenSpace.x)
+        {
+            playerObject.transform.localScale = new Vector3(-playerObject.transform.localScale.z, playerObject.transform.localScale.y, playerObject.transform.localScale.z);
+        }
+
+        if (Input.mousePosition.x > playerScreenSpace.x)
+        {
+            playerObject.transform.localScale = new Vector3(playerObject.transform.localScale.z, playerObject.transform.localScale.y, playerObject.transform.localScale.z);
+        }
+
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "GOON" && isAttacking)
+        {
+            Debug.Log("enemy hit");
+            GoonScript goonHit = collision.gameObject.GetComponent<GoonScript>();
+            goonHit.goonHealth -= tempDamage;
+            goonHit.healthBar.value = goonHit.goonHealth;
+
+
+        }
+    }
+
+    public IEnumerator attackBool()
+    {
+        isAttacking = true;
+        yield return new WaitForSeconds(0.5f);
+        isAttacking = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interactable"))
+        {
+            iText = other.gameObject.GetComponent<IShowText>();
+            iText.ShowText(true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interactable"))
+        {                       
+            Debug.Log("Tag matches");   
+            canInteract = true;
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                Interact(other.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interactable"))
+        {
+            iText.ShowText(false);
+        }
+
+    }
+
+    void Interact(GameObject InteractableObject)
+    {
+        Debug.Log("interact on character called");
+        IInteractable i = InteractableObject.GetComponent<IInteractable>();
+
+        {
+            i.Action();
+            
+            anim.SetTrigger("Attack");
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Sliceable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sliceable : MonoBehaviour, IInteractable, IShowText
+{
+    public bool sliceable;
+    public GameObject Top;
+    public GameObject sliceText;
+    Camera cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Top = gameObject.transform.Find("Top").gameObject;
+        sliceText = gameObject.transform.Find("canvas").gameObject.transform.Find("slicetext").gameObject;
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sliceText.transform.LookAt(cam.transform, Vector3.up);
+    }
+
+    public void Action()
+    {
+        Debug.Log("Action on tree called");
+        Slice();
+    }
+
+    public void Slice()
+    {
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        Top.GetComponent<Rigidbody>().isKinematic = false;
+        ShowText(false);
+    }
+
+    public void ShowText(bool show)
+    {
+        sliceText.GetComponent<Text>().enabled = show;
+    }
+
+}
